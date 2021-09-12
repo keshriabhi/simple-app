4cfecd6dbfb48c15e1371698ab1eaa715e0c4dd2
keshriabhi <abhishekkeshri@live.com>
2021-09-12 22:46:57 +0530

    second committ

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('Simple app console')
+
+
+git init
+git remote add origin https://github.com/keshriabhi/simple-app.git
+git branch -M main
+git add 
+git commit -m ''
+git push -u origin main
